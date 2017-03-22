3c44480934c4e1087a5189f203a3de550f583b65
Johntavious Barkley <jbarkley3@gatech.edu>
2017-03-22 01:55:39 -0400

    firebase stuff

--- app/src/main/java/com/abusement/park/acneed/activity/WelcomeActivity.java
+++ app/src/main/java/com/abusement/park/acneed/activity/WelcomeActivity.java
@@ -1,11 +1,7 @@
 package com.abusement.park.acneed.activity;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.TextView;
 
 import com.abusement.park.acneed.R;
