d19d76a41dd2d90344fac43f53484a0a4511c0c4
Johntavious Barkley <jbarkley3@gatech.edu>
2017-03-25 18:23:04 -0400

    able to compress images now

--- app/src/main/java/com/abusement/park/acneed/activity/WelcomeActivity.java
+++ app/src/main/java/com/abusement/park/acneed/activity/WelcomeActivity.java
@@ -3,19 +3,27 @@ package com.abusement.park.acneed.activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
-import android.widget.SpinnerAdapter;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.abusement.park.acneed.R;
+import com.abusement.park.acneed.utils.ImageCompressor;
 import com.google.firebase.auth.FirebaseAuth;
 
 import org.apache.commons.lang3.StringUtils;
 
+import java.io.FileNotFoundException;
+
 public class WelcomeActivity extends AppCompatActivity {
+
+    private static final String TAG = "WELCOME";
 
     private FirebaseAuth firebaseAuth;
 
@@ -24,11 +32,16 @@ public class WelcomeActivity extends AppCompatActivity {
     private TextView editReminderSettingsTextView;
     private TextView saveReminderSettingsTextView;
     private TextView usernameText;
+    private LinearLayout thumbnailsLayout;
 
     private String currentReminderFrequency;
     private int currentReminderInterval;
 
     private static final String[] TIME_INTERVALS = {"days", "weeks", "months"};
+    private static final int CHOOSE_IMAGE_REQUEST_CODE = 1;
+    private static final int THUMBNAIL_HEIGHT = 100;
+    private static final int THUMBNAIL_WIDTH = 100;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -54,6 +67,7 @@ public class WelcomeActivity extends AppCompatActivity {
         timeIntervalSpinner = (Spinner) findViewById(R.id.Home_interval_spinner);
         editReminderSettingsTextView = (TextView) findViewById(R.id.Home_edit_reminder_edit_text);
         saveReminderSettingsTextView = (TextView) findViewById(R.id.Home_save_reminder_changes);
+        thumbnailsLayout = (LinearLayout) findViewById(R.id.Home_scroll_view_layout);
 
         timeIntervalSpinner.setAdapter(new ArrayAdapter<>(this, android.R.layout
                 .simple_spinner_item, TIME_INTERVALS));
@@ -65,7 +79,29 @@ public class WelcomeActivity extends AppCompatActivity {
     }
 
     public void addImageToMyJourney(View view) {
+        Intent chooseImage = new Intent().setType("image/*").setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(chooseImage, "Select Image to Add to " +
+                "Journey"), CHOOSE_IMAGE_REQUEST_CODE);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CHOOSE_IMAGE_REQUEST_CODE && resultCode == RESULT_OK) {
+            Log.d(TAG, "Choose Image Request completed with data " + data.getData());
+            ImageView newThumbnail = new ImageView(this);
+            Log.d(TAG, "Attempting to compress Image");
+            try {
+                newThumbnail.setImageBitmap(ImageCompressor.compressImageToThumbnail(data.getData(),
+                        getContentResolver(), THUMBNAIL_WIDTH, THUMBNAIL_HEIGHT));
+            } catch (FileNotFoundException e) {
+                Log.d(TAG, "Error compressing image", e);
+                Toast.makeText(this, "Failed to upload image.", Toast.LENGTH_LONG).show();
+            }
+            Log.d(TAG, "Image compression succeeded. Setting layout and adding to layout");
+            newThumbnail.setLayoutParams(new LinearLayout.LayoutParams(THUMBNAIL_WIDTH,
+                    THUMBNAIL_HEIGHT));
+            thumbnailsLayout.addView(newThumbnail);
+        }
     }
 
     public void editReminderSettings(View view) {
@@ -102,7 +138,6 @@ public class WelcomeActivity extends AppCompatActivity {
             editReminderSettingsTextView.setText("edit");
             // todo need to update the database with new settings
             // probably use an AsyncTask
-
         }
     }
 
---
+++ app/src/main/java/com/abusement/park/acneed/utils/ImageCompressor.java
@@ -0,0 +1,32 @@
+package com.abusement.park.acneed.utils;
+
+import android.content.ContentResolver;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.media.ThumbnailUtils;
+import android.net.Uri;
+
+import java.io.FileNotFoundException;
+
+public class ImageCompressor {
+
+
+    /**
+     * Needed in order to avoid OutOfMemoryExceptions
+     * @param filepath
+     * @param contentResolver
+     * @param width
+     * @param height
+     * @return
+     * @throws FileNotFoundException
+     */
+    public static Bitmap compressImageToThumbnail(Uri filepath, ContentResolver contentResolver, int width, int height)
+            throws FileNotFoundException {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inTempStorage = new byte[24 * 1024];
+        options.inJustDecodeBounds = false;
+        options.inSampleSize=4;
+        Bitmap bitmap = BitmapFactory.decodeStream(contentResolver.openInputStream(filepath), null, options);
+        return ThumbnailUtils.extractThumbnail(bitmap, width, height);
+    }
+}
