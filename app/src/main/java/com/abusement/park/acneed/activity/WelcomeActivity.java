51b96e41e3b168c7e5ab080c8d26775cca8f6fe1
Johntavious Barkley <jbarkley3@gatech.edu>
2017-03-25 02:12:59 -0400

    Registration / login checkpoint

--- app/src/main/java/com/abusement/park/acneed/activity/WelcomeActivity.java
+++ app/src/main/java/com/abusement/park/acneed/activity/WelcomeActivity.java
@@ -1,27 +1,28 @@
 package com.abusement.park.acneed.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
 import com.abusement.park.acneed.R;
-
-import org.apache.commons.lang3.StringUtils;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class WelcomeActivity extends AppCompatActivity {
 
-    private String username;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
-        String username = getIntent().getExtras().getString("user");
-        if (StringUtils.isNotBlank(username)) {
-            TextView usernameText = (TextView) findViewById(R.id
-                    .Welcome_username);
-            usernameText.setText("Welcome, " + username);
+        firebaseAuth = FirebaseAuth.getInstance();
+        if (firebaseAuth.getCurrentUser() == null) {
+            finish();
+            startActivity(new Intent(this, LoginActivity.class));
         }
+        TextView usernameText = (TextView) findViewById(R.id.Welcome_username);
+        usernameText.setText("Welcome, " + firebaseAuth.getCurrentUser().getEmail());
     }
 
 }
